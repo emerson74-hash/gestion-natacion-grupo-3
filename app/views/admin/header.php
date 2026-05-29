e5532363e703e2281acde0f046edb7bae3e11e12
nereacb26 <braganerea06@gmail.com>
2026-05-29 20:45:34 -0300

    fix: agregado de estilos globales al header admin

--- app/views/admin/header.php
+++ app/views/admin/header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
 
-    <title>Panel Administrativo</title>
+    <title><?= $titulo ?? 'Panel Administrativo' ?></title>
 
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-   
-    <link rel="stylesheet" href="<?= _URL ?>/assets/css/admin.css">
 
-    
+    <!-- Estilos globales -->
+    <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/style.css">
+
+    <!-- Estilos admin -->
+    <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/admin.css">
 
 </head>
 
 <body>
-
