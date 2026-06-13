8a6a86837548e2662517efed76a449654228ff3a
Emerson <emerson74_@hotmail.com>
2026-06-12 21:18:37 -0300

    fix scrip removido de vistas

--- public/js/modules/authMain.js
+++ public/js/modules/authMain.js
@@ -3,10 +3,12 @@ import { initRegister } from "./auth/formRegister.js";
 import { initForgotPassword } from "./auth/formForgotPassword.js";
 import { initResetPassword } from "./auth/formResetPassword.js";
 import { initEdit } from "./coach/formEdit.js";
-import { initLessonsTable } from "./coach/tableLessons.js";
+import { initLessonsTableCoach } from "./coach/tableLessons.js";
 import { initSwimmerProfile } from "./swimmer/formProfile.js";
 import { initCoachesTable } from "./admin/tableCoaches.js";
 import { initDeleteCoach } from "./admin/deleteCoach.js";
+import { initLessonsTableAdmin } from "./admin/tableLessons.js";
+import { initDeleteLesson } from "./admin/deleteLesson.js";
 
 
 // Esperamos a que el DOM esté completamente cargado para evitar errores de referencia
@@ -25,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Funciones del coach
     initEdit();
-    initLessonsTable();
+    initLessonsTableCoach();
 
     // Funciones del swimmer
     initSwimmerProfile();
@@ -33,7 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
     //funciones de admin
     initCoachesTable();
     initDeleteCoach();
-
+    initLessonsTableAdmin();
+    initDeleteLesson();
 
     console.log("Auth module initialized successfully."); 
 });
