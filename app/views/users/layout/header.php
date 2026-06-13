b7b4cf2fbec56ff82ec7fb25a92560dade50ca7f
Emerson <emerson74_@hotmail.com>
2026-06-13 02:29:24 -0300

    foter bajado para todas las vistas

--- app/views/users/layout/header.php
+++ app/views/users/layout/header.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
-<html lang="es">
-<meta name="base-url" content="<?= rtrim(Env::get('APP_URL'), '/') ?>">
-</head>
+<html lang="es"></html>
+
+<head>
+    <meta name="base-url" content="<?= rtrim(Env::get('APP_URL'), '/') ?>">
+
+    <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/toLowerFooter.css">
+
     <meta charset="UTF-8">
     <title><?= $titulo ?? 'SWIM LEARN' ?></title>
 
@@ -12,11 +16,9 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
@@ -25,49 +27,49 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.2/cropper.min.css">
-    
-   
-<?php
-$url = $_GET['url'] ?? '';
-
-if ($url == '' || $url == 'home') {
-
-?>
-
-<link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/landing.css">
 
 
-<?php
+    <?php
+    $url = $_GET['url'] ?? '';
 
-} elseif (
-    $url == 'login' ||
-    $url == 'register' ||
-    $url == 'forgot-password' ||
-    $url == 'reset-password'
-) {
+    if ($url == '' || $url == 'home') {
 
-?>
+        ?>
 
-<link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/auth.css">
+        <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/landing.css">
 
 
-<?php
+        <?php
 
-} else {
+    } elseif (
+        $url == 'login' ||
+        $url == 'register' ||
+        $url == 'forgot-password' ||
+        $url == 'reset-password'
+    ) {
 
-?>
+        ?>
 
-<link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/app.css">
+        <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/auth.css">
 
 
-<?php
+        <?php
 
-}
+    } else {
 
-?>
+        ?>
 
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
-<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+        <link rel="stylesheet" href="<?= Env::get('ASSET_URL') ?>/assets/css/app.css">
+
+
+        <?php
+
+    }
+
+    ?>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
 
     <style>
         .navbar {
@@ -85,7 +87,8 @@ if ($url == '' || $url == 'home') {
         }
 
         .navbar-brand img {
-            height: 50px; /* Ajustá si querés más grande o más chico */
+            height: 50px;
+            /* Ajustá si querés más grande o más chico */
             width: auto;
             display: block;
         }
@@ -104,89 +107,84 @@ if ($url == '' || $url == 'home') {
             border-radius: 50%;
             border: 2px solid #4FD1E8;
         }
+
         .navbar-nav {
-    margin-right: 35px;
-}
+            margin-right: 35px;
+        }
     </style>
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid px-5">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container-fluid px-5">
+                <a class="navbar-brand" href="<?=
+                    match ($_SESSION['role_id'] ?? 0) {
+                        1 => '?url=admin&section=dashboard',
+                        2 => '?url=coach/dashboard',
+                        3 => '?url=swimmer/dashboard',
+                        default => '?url=landing'
+                    }
+                    ?>">
 
-        <a class="navbar-brand" href="<?=
-            match ($_SESSION['role_id'] ?? 0) {
-                1 => '?url=admin&section=dashboard',
-                2 => '?url=coach/dashboard',
-                3 => '?url=swimmer/dashboard',
-                default => '?url=landing'
-            }
-        ?>">
+                    <img src="/Gestion-Natacion-Grupo-3/public/imglogo/logo.png" alt="SWIM LEARN">
 
-            <img
-                src="/Gestion-Natacion-Grupo-3/public/imglogo/logo.png"
-                alt="SWIM LEARN">
+                    <span class="text-swim">
+                        SWIM LEARN
+                    </span>
 
-            <span class="text-swim">
-                SWIM LEARN
-            </span>
+                </a>
 
-        </a>
+                <div class="collapse navbar-collapse">
 
-        <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ms-auto align-items-center">
 
-            <ul class="navbar-nav ms-auto align-items-center">
+                        <?php if (isset($_SESSION['user_id'])): ?>
 
-                <?php if (isset($_SESSION['user_id'])): ?>
+                            <?php
+                            $foto = $_SESSION['profile_image'] ?? 'default-profile.png';
 
-                    <?php
-                    $foto = $_SESSION['profile_image'] ?? 'default-profile.png';
+                            $rutaFoto =
+                                Env::get('ASSET_URL')
+                                . "/img/uploads/profiles/"
+                                . $foto;
+                            ?>
 
-                    $rutaFoto =
-                        Env::get('ASSET_URL')
-                        . "/img/uploads/profiles/"
-                        . $foto;
-                    ?>
+                            <li class="nav-item d-flex align-items-center">
 
-                    <li class="nav-item d-flex align-items-center">
+                                <img src="<?= $rutaFoto ?>" alt="Perfil" class="profile-img-nav me-2">
 
-                        <img
-                            src="<?= $rutaFoto ?>"
-                            alt="Perfil"
-                            class="profile-img-nav me-2">
+                                <span class="nav-link text-info p-0">
+                                    Hola
+                                    <?= htmlspecialchars($_SESSION['first_name'] ?? '') ?>
+                                </span>
 
-                        <span class="nav-link text-info p-0">
-                            Hola
-                            <?= htmlspecialchars($_SESSION['first_name'] ?? '') ?>
-                        </span>
+                            </li>
 
-                    </li>
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-outline-info btn-sm ms-3" href="?url=logout">
+                                    Salir
+                                </a>
+                            </li>
 
-                    <li class="nav-item">
-                        <a
-                            class="nav-link btn btn-outline-info btn-sm ms-3"
-                            href="?url=logout">
-                            Salir
-                        </a>
-                    </li>
+                        <?php else: ?>
 
-                <?php else: ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="?url=login">
+                                    Ingresar
+                                </a>
+                            </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="?url=login">
-                            Ingresar
-                        </a>
-                    </li>
+                        <?php endif; ?>
 
-                <?php endif; ?>
+                    </ul>
 
-            </ul>
+                </div>
 
-        </div>
+            </div>
 
-    </div>
+        </nav>
+    </header>
 
-</nav>
-
-<main>
+    <main>
